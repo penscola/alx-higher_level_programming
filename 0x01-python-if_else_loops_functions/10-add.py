fb21abdd3f34129b5db0a2ddf639da443d55a2c3
penscola <penscolashackletonfelix@gmail.com>
2023-06-06 22:06:09 +0300

    a + b

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """Adds two integers
+    Args:
+        a (_integer_)
+        b (_integer_)
+    Returns:
+        _integer_: Returns the sum of two integers
+    """
+    return a + b
