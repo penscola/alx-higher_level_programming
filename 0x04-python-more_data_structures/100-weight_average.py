8ce822fb19effb82b36a7c1844224c9a5e129d17
penscola <penscolashackletonfelix@gmail.com>
2023-06-14 20:41:21 +0300

    chore: Weighted average!

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list is None or len(my_list) == 0:
+        return 0
+    return sum(map(lambda x: x[0] * x[1], my_list)) / sum(map(lambda x: x[1], my_list))
