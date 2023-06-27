df8b3827d9881c8952931e21bff7afcf0b933945
penscola <penscolashackletonfelix@gmail.com>
2023-06-27 20:46:35 +0300

    chore: Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module defines a class Square"""
+
+
+class Square:
+    """Square class with a private attribute -
+    size.
+    """
+
+    def __init__(self, size):
+        """Initializes the data."""
+        self.__size = size
