601b1365ebe99ae006a0bbb206b1b2952c910e5b
penscola <penscolashackletonfelix@gmail.com>
2023-07-03 19:42:11 +0300

    chore:  Simple rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines an Empty Rectangle class"""
+
+
+class Rectangle:
+    """Empty Rectangle class"""
+    pass
+
+
+if __name__ == "__main__":
+    my_rectangle = Rectangle()
+    print(type(my_rectangle))
+    print(my_rectangle.__dict__)
