8263e590bbe69ff4c331ab1c93b162c6d99d678d
penscola <penscolashackletonfelix@gmail.com>
2023-07-10 20:25:07 +0300

    chore: Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Empty BaseGeometry class
+
+
+This Module is for the project 0x0A. Python - Inheritance
+proposed by Holberton school as a test for the design of
+BaseGeometry class.
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry empty class
+
+    Args:
+        None
+
+    Attributes:
+        None
+
+    """
+
+    def area(self):
+        """Class area.
+
+        Args:
+            None
+
+        Returns:
+            Exception
+
+        """
+        raise Exception("area() is not implemented")
