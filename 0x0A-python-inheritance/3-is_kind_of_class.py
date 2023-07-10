8cddad91cfa95131d1e9e0265059e941e49b188c
penscola <penscolashackletonfelix@gmail.com>
2023-07-10 20:15:23 +0300

    chore: Same class or inherit from

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Module that returns the list of available attributes
+and methods of an object.
+
+
+This Module is for the project 0x0A. Python - Inheritance
+proposed by Holberton school as a test for the design of
+method that returns the list of available attributes and
+methods of an object.
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    is_same_class function returns True if the object is exactly an instance
+    """
+
+    return isinstance(obj, a_class)
