d1d07aa7c92d50fde975b1cbe88dd2490831f4d0
penscola <penscolashackletonfelix@gmail.com>
2023-07-10 20:10:02 +0300

    chore: Exact same object

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
+""" Module that returns the list of available attributes
+and methods of an object.
+
+
+This Module is for the project 0x0A. Python - Inheritance
+proposed by Holberton school as a test for the design of
+method that returns the list of available attributes and
+methods of an object.
+"""
 
 
 def is_same_class(obj, a_class):
-    """ returns True if the object is exactly an instance of the specified
-    class ; otherwise False."""
+    """
+    is_same_class function returns True if the object is exactly an instance
+    """
+
     if type(obj) is a_class:
         return True
-    else:
-        return False
+    return False
