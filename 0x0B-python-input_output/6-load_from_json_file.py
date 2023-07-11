508764702dbff5b889ebcc8bd98e039f3437138a
penscola <penscolashackletonfelix@gmail.com>
2023-07-11 19:08:26 +0300

    chore: Create object from a JSON file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module contains load_from_json_file"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """ creates an Object from a json file"""
+    with open(filename, 'r', encoding='utf-8') as f:
+        return json.load(f)
