acda07f134351ed499642b0833e3c8497b6ebcc8
penscola <penscolashackletonfelix@gmail.com>
2023-09-30 09:24:29 +0300

    :bug: fixed minors

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#takes in a URL, sends a request to that URL,and displays the size of the body of the response
-curl -s "$1" | wc -c
+# Takes a URL, sends a request and displays the size of the body
+curl -sI $1 | grep -i content-length | awk '{print $2}'
